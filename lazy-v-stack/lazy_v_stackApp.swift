7b9ec17c3266e7b73edde5528b71f55845ee3d0e
Paul Denlinger <pmdenlinger@icloud.com>
2021-06-23 18:52:15 -0700

    Initial Commit

---
+++ lazy-v-stack/lazy_v_stackApp.swift
@@ -0,0 +1,17 @@
+//
+//  lazy_v_stackApp.swift
+//  lazy-v-stack
+//
+//  Created by user on 6/23/21.
+//
+
+import SwiftUI
+
+@main
+struct lazy_v_stackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
